7b83399cb74ecc46a1cb8086b3f1da3c53deca60
arghaghosh <argha7417@gmail.com>
2023-10-19 08:46:58 +0530

    Made a standard looking navbar

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <span className="navbar-logo">Portfolio</span>
+        <button className="navbar-toggler" onClick={toggleMenu}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div className={`navbar-menu ${isOpen ? 'active' : ''}`}>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a href="#" className="nav-link">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">About</a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">Services</a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
-
+export default Navbar;
