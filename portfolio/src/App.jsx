48a8a4c038283c779ac9422b6411e41071a50ad9
Diya Ghosh <diyaag2208@gmail.com>
2023-10-26 19:48:58 +0530

    Hero banner designed

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 // import React from "react";
 import Navbar from "./components/Navbar";
+import HeroBanner from "./components/HeroBanner";
 
 function App()
 {
     return (
         <>
             <Navbar/>
+            <HeroBanner/>
         </>
     )
 
